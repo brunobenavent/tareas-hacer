9bab7e996d5ca225a3b2b9764bd72e5405b4e400
Bruno Benavent Colomo <70907703+brunobenavent@users.noreply.github.com>
2023-04-03 14:56:15 +0200

    primer commit

---
+++ app.js
@@ -0,0 +1,20 @@
+require ('colors')
+const {mostrarMenu, pausa} = require('./helpers/mensajes.js');
+console.clear()
+
+const main = async() =>{
+    
+    let opt= ''
+
+    do {
+        opt = await mostrarMenu();
+        console.log({opt})
+        await pausa();
+        
+    } while (opt!=='0');
+
+
+
+ 
+}
+main();
